6dabaeef4c976139b380c5bd0b8e3d7eeca96252
Anay <anayshah29@gmail.com>
2024-12-17 16:08:09 -0500

    Updates

---
+++ precache-manifest.e510ad1003fdf8b53f2e1cd04e5cebe0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6801c214183ee5881ce888d9a0a6dec0",
+    "url": "/sample/index.html"
+  },
+  {
+    "revision": "00685493ef539cdeaa86",
+    "url": "/sample/static/css/main.4c719808.chunk.css"
+  },
+  {
+    "revision": "466799877f10c19b29a2",
+    "url": "/sample/static/js/2.d1eb0c69.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/sample/static/js/2.d1eb0c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00685493ef539cdeaa86",
+    "url": "/sample/static/js/main.fec47cf8.chunk.js"
+  },
+  {
+    "revision": "a7ac0730fd017d0f4e59",
+    "url": "/sample/static/js/runtime-main.6ec3bd77.js"
+  },
+  {
+    "revision": "cc357a160293e0a4b82f1ded3d1e4db4",
+    "url": "/sample/static/media/Tanjiro.cc357a16.png"
+  }
+]);
